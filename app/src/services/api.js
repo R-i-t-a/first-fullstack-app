cb30e9e779a2027aa97ac0eacd684c7da49fcb60
Claire Flanagan <claireflanagan@gmail.com>
2018-08-29 11:31:26 -0700

    errors fixed

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,12 +1,12 @@
 export default {
   getWhales() {
-    return fetch('http://localhost:8080/api/whales', {
+    return fetch('http://localhost:3000/api/whales', {
       headers: { 'Content-Type': 'application/json' }
     })
       .then(response => response.json()); 
   },
   addWhale(whale) {
-    return fetch('http://localhost:8080/api/whales', {
+    return fetch('http://localhost:3000/api/whales', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(whale)
--- server/server.js
+++ server/server.js
@@ -14,18 +14,22 @@ const dataPath = 'data/whales.json';
 
 function readData() {
   const raw = fs.readFileSync(dataPath);
-  const data = JSON.parse(raw);
+  const data = JSON.parse(raw); 
   
   return data;
 }
 
 app.get('/api/whales', (req, res) => {
   const data = readData();
+  res.send(data);
+});
+
+app.post('/api/whales', (req, res) => {
+  const data = readData();
   data.push(req.body);
   fs.writeFileSync(dataPath, JSON.stringify(data));
-
   res.send(req.body);
-});
+})
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('app running...'));
